39c2e598ae8dbf1cbb2937b54e3ec5e4c803cb6b
gloryan0829 <candoublej@gmail.com>
2023-02-15 17:59:32 +0900

    updated with go relayer hash 00afe149d0c78492deba580776c651805ea0dc05

--- local.Dockerfile
+++ local.Dockerfile
@@ -1,54 +1,34 @@
-FROM golang:1.19-alpine3.16 AS build-env
+FROM golang:1.18-alpine as BUILD
 
-RUN apk add --update --no-cache curl make git libc-dev bash gcc linux-headers eudev-dev
+WORKDIR /relayer
 
-ADD . .
+# Update and install needed deps prioir to installing the binary.
+RUN apk update && \
+  apk --no-cache add make git build-base
 
-RUN CGO_ENABLED=1 LDFLAGS='-linkmode external -extldflags "-static"' make install
+# Copy go.mod and go.sum first and download for caching go modules
+COPY go.mod go.mod
+COPY go.sum go.sum
 
-# Use minimal busybox from infra-toolkit image for final scratch image
-FROM ghcr.io/strangelove-ventures/infra-toolkit:v0.0.6 AS busybox-min
-RUN addgroup --gid 1000 -S relayer && adduser --uid 100 -S relayer -G relayer
+RUN go mod download
 
-# Use ln and rm from full featured busybox for assembling final image
-FROM busybox:1.34.1-musl AS busybox-full
+# Copy the files from host
+COPY . .
 
-# Build final image from scratch
-FROM scratch
+RUN make install
 
-LABEL org.opencontainers.image.source="https://github.com/cosmos/relayer"
+FROM alpine:latest
 
-WORKDIR /bin
+ENV RELAYER /relayer
 
-# Install ln (for making hard links) and rm (for cleanup) from full busybox image (will be deleted, only needed for image assembly)
-COPY --from=busybox-full /bin/ln /bin/rm ./
+RUN addgroup rlyuser && adduser -S -G rlyuser rlyuser -h "$RELAYER"
 
-# Install minimal busybox image as shell binary (will create hardlinks for the rest of the binaries to this data)
-COPY --from=busybox-min /busybox/busybox /bin/sh
+USER rlyuser
 
-# Add hard links for read-only utils, then remove ln and rm
-# Will then only have one copy of the busybox minimal binary file with all utils pointing to the same underlying inode
-RUN ln sh pwd && \
-    ln sh ls && \
-    ln sh cat && \
-    ln sh less && \
-    ln sh grep && \
-    ln sh sleep && \
-    ln sh env && \
-    ln sh tar && \
-    ln sh tee && \
-    ln sh du && \
-    rm ln rm
+# Define working directory
+WORKDIR $RELAYER
 
-# Install chain binaries
-COPY --from=build-env /bin/rly /bin
+# Copy binary from BUILD
+COPY --from=BUILD /go/bin/rly /usr/bin/rly
 
-# Install trusted CA certificates
-COPY --from=busybox-min /etc/ssl/cert.pem /etc/ssl/cert.pem
-
-# Install relayer user
-COPY --from=busybox-min /etc/passwd /etc/passwd
-COPY --from=busybox-min --chown=100:1000 /home/relayer /home/relayer
-
-WORKDIR /home/relayer
-USER relayer
+ENTRYPOINT ["/usr/bin/rly"]
--- relayer/chains/cosmos/provider.go
+++ relayer/chains/cosmos/provider.go
@@ -3,6 +3,7 @@ package cosmos
 import (
 	"context"
 	"fmt"
+	"github.com/soohoio/stayking/x/interchainquery"
 	"os"
 	"sync"
 	"time"
@@ -27,20 +28,19 @@ var (
 )
 
 type CosmosProviderConfig struct {
-	Key            string   `json:"key" yaml:"key"`
-	ChainName      string   `json:"-" yaml:"-"`
-	ChainID        string   `json:"chain-id" yaml:"chain-id"`
-	RPCAddr        string   `json:"rpc-addr" yaml:"rpc-addr"`
-	AccountPrefix  string   `json:"account-prefix" yaml:"account-prefix"`
-	KeyringBackend string   `json:"keyring-backend" yaml:"keyring-backend"`
-	GasAdjustment  float64  `json:"gas-adjustment" yaml:"gas-adjustment"`
-	GasPrices      string   `json:"gas-prices" yaml:"gas-prices"`
-	MinGasAmount   uint64   `json:"min-gas-amount" yaml:"min-gas-amount"`
-	Debug          bool     `json:"debug" yaml:"debug"`
-	Timeout        string   `json:"timeout" yaml:"timeout"`
-	OutputFormat   string   `json:"output-format" yaml:"output-format"`
-	SignModeStr    string   `json:"sign-mode" yaml:"sign-mode"`
-	ExtraCodecs    []string `json:"extra-codecs" yaml:"extra-codecs"`
+	Key            string  `json:"key" yaml:"key"`
+	ChainName      string  `json:"-" yaml:"-"`
+	ChainID        string  `json:"chain-id" yaml:"chain-id"`
+	RPCAddr        string  `json:"rpc-addr" yaml:"rpc-addr"`
+	AccountPrefix  string  `json:"account-prefix" yaml:"account-prefix"`
+	KeyringBackend string  `json:"keyring-backend" yaml:"keyring-backend"`
+	GasAdjustment  float64 `json:"gas-adjustment" yaml:"gas-adjustment"`
+	GasPrices      string  `json:"gas-prices" yaml:"gas-prices"`
+	MinGasAmount   uint64  `json:"min-gas-amount" yaml:"min-gas-amount"`
+	Debug          bool    `json:"debug" yaml:"debug"`
+	Timeout        string  `json:"timeout" yaml:"timeout"`
+	OutputFormat   string  `json:"output-format" yaml:"output-format"`
+	SignModeStr    string  `json:"sign-mode" yaml:"sign-mode"`
 }
 
 func (pc CosmosProviderConfig) Validate() error {
@@ -77,6 +77,9 @@ func (pc CosmosProviderConfig) NewProvider(log *zap.Logger, homepath string, deb
 // ChainClientConfig builds a ChainClientConfig struct from a CosmosProviderConfig, this is used
 // to instantiate an instance of ChainClient from lens which is how we build the CosmosProvider
 func ChainClientConfig(pcfg *CosmosProviderConfig) *lens.ChainClientConfig {
+	var modules []module.AppModuleBasic
+	modules = append(modules, lens.ModuleBasics...)
+	modules = append(modules, interchainquery.AppModuleBasic{})
 	return &lens.ChainClientConfig{
 		Key:            pcfg.Key,
 		ChainID:        pcfg.ChainID,
@@ -90,8 +93,7 @@ func ChainClientConfig(pcfg *CosmosProviderConfig) *lens.ChainClientConfig {
 		Timeout:        pcfg.Timeout,
 		OutputFormat:   pcfg.OutputFormat,
 		SignModeStr:    pcfg.SignModeStr,
-		ExtraCodecs:    pcfg.ExtraCodecs,
-		Modules:        append([]module.AppModuleBasic{}, lens.ModuleBasics...),
+		Modules:        modules,
 	}
 }
 
